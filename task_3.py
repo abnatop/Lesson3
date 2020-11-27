1a5cb18f461bce404fc17006321abf22c8f98c5a
abnatop <abnatop@gmail.com>
2020-11-27 20:13:32 +0900

    Задача 3.

--- task_3.py
+++ task_3.py
@@ -1,2 +1,9 @@
 # Реализовать функцию my_func(), которая принимает три позиционных аргумента, и
 # возвращает сумму наибольших двух аргументов.
+
+def my_func(arg_one, arg_two, arg_three):
+    ascending_order = [arg_one, arg_two, arg_three]
+    ascending_order.sort()
+    return ascending_order[1] + ascending_order[2]
+
+print(my_func(10, 5, 1))
