9400f1c4543c5894d2de560ef6a9712bc7a14e86
abnatop <abnatop@gmail.com>
2020-11-27 21:53:33 +0900

    Задача 5.

--- task_5.py
+++ task_5.py
@@ -6,3 +6,17 @@
 # чисел, то вначале нужно добавить сумму этих чисел к полученной ранее сумме и после этого
 # завершить программу.
 
+EXIT_CODE = '#'
+exit_symbol = ''
+
+result = 0
+
+while exit_symbol != EXIT_CODE:
+    digits = input('Введите строку чисел, разделенных пробелом (или # для выхода): ')
+    if EXIT_CODE in digits:
+        digits = digits.replace(EXIT_CODE, '')
+        exit_symbol = EXIT_CODE
+
+    digits_list = [float(digit) for digit in digits.split()]
+    result += sum(digits_list)
+    print(f'Сумма введенных чисел равна {result}.')
