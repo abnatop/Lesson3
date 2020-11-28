9b8a49d45716644f89a3d0fea00d9fbba2b68a63
abnatop <abnatop@gmail.com>
2020-11-28 11:08:46 +0900

    Задача 6. Реализация вручную.

--- task_6.py
+++ task_6.py
@@ -6,8 +6,22 @@
 # вывод исходной строки, но каждое слово должно начинаться с заглавной буквы. Необходимо
 # использовать написанную ранее функцию int_func().
 
-def int_func(text):
-    return text.title()
+# def int_func(text):
+#     return text.title()
+#
+# print(int_func('text'))
+# print(int_func('some words here.'))
 
-print(int_func('text'))
-print(int_func('some words here.'))
+# самостоятельная реализация
+CASE_OFFSET = 32
+
+def to_upper_first_ltr(text):
+    return chr(ord(text[0]) - CASE_OFFSET) + text[1:]
+
+def int_func(text):
+    text_words = text.split()
+    for i in range(len(text_words)):
+        text_words[i] = to_upper_first_ltr(text_words[i])
+    return ' '.join(text_words)
+
+print(int_func('one two three alpha beta gamma'))
